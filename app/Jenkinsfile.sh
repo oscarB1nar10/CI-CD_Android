9aa25310e2768768088bdf137265c3e709abcd10
Oscar <oscivaramtin@gmail.com>
2022-04-29 15:46:13 -0500

    Jenkins file included in order to build from Jenkins

---
+++ app/Jenkinsfile.sh
@@ -0,0 +1,55 @@
+def err = null
+try {
+
+    node {
+
+        stage('Preparation') {
+            git credentialsId: '24a11330-a387-470c-8268-d46cdfef2271', url: 'https://github.com/oscarB1nar10/CI-CD_Android.git'
+        }
+
+        stage('Dependencies') {
+                #sh 'sudo npm install -g react-native-cli'
+                #sh 'npm install'
+                #sh 'react-native link'
+                sh 'export JAVA_HOME=/opt/jdk1.8.0_201'
+                sh 'export JRE_HOME=/opt/jdk1.8.0_201/jre'
+                sh 'export PATH=$PATH:/opt/jdk1.8.0_201/bin:/opt/jdk1.8.0_201/jre/bin'
+                sh 'echo $JAVA_HOME'
+        }
+
+        stage('Clean Build') {
+                dir("android") {
+                    sh "pwd"
+                    sh 'ls -al'
+                    sh './gradlew clean'
+                }
+        }
+
+        stage('Build release ') {
+            parameters {
+                credentials credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.FileCredentialsImpl', defaultValue: '24a11330-a387-470c-8268-d46cdfef2271', description: '', name: 'keystore', required: true
+            }
+            dir("android") {
+                sh './gradlew assembleRelease'
+            }
+        }
+
+        stage('Compile') {
+            archiveArtifacts artifacts: '**/*.apk', fingerprint: true, onlyIfSuccessful: true
+        }
+    }
+
+} catch (caughtError) {
+
+    err = caughtError
+    currentBuild.result = "FAILURE"
+
+} finally {
+
+    if(currentBuild.result == "FAILURE"){
+              sh "echo 'Build FAILURE'"
+    }else{
+         sh "echo 'Build SUCCESSFUL'"
+    }
+
+}
